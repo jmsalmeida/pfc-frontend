f175b0bd47699329b0f2e52d910c8082d3ded426
James Almeida <james.almeida.ti@gmail.com>
2022-04-29 23:34:31 -0300

    Add ui-kitten ui library

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import * as eva from '@eva-design/eva';
+import { StyleSheet } from 'react-native';
+import { ApplicationProvider, Layout, Text, CheckBox } from '@ui-kitten/components';
 
 export default function App() {
+  const [checked, setChecked] = React.useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <Layout style={styles.container}>
+        <Text category='h1'>HOME</Text>
+      </Layout>
+    </ApplicationProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },

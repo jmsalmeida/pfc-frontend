0fb3d01d82075847af52617c95ea8bfc875afa90
James Almeida <james.almeida.ti@gmail.com>
2022-04-30 00:45:26 -0300

    Add react navigation with example

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { StyleSheet } from 'react-native';
-import { ApplicationProvider, Layout, Text, CheckBox } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { AppNavigator } from './src/Navigators/navigation.component';
 
 export default function App() {
-  const [checked, setChecked] = React.useState(false);
-
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <Layout style={styles.container}>
-        <Text category='h1'>HOME</Text>
-      </Layout>
-    </ApplicationProvider>
+    <>
+      <IconRegistry icons={EvaIconsPack}/>
+
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <AppNavigator/>
+      </ApplicationProvider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

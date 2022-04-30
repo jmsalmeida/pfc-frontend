2702d88a727be1fdcb10d28a1e37bc6aaefd892f
James Almeida <james.almeida.ti@gmail.com>
2022-04-30 19:52:32 -0300

    Add react-query configuration

--- App.js
+++ App.js
@@ -4,14 +4,18 @@ import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { AppNavigator } from './src/Navigators/navigation.component';
 
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 export default function App() {
+  const queryClient = new QueryClient()
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <IconRegistry icons={EvaIconsPack}/>
 
       <ApplicationProvider {...eva} theme={eva.light}>
         <AppNavigator/>
       </ApplicationProvider>
-    </>
+    </QueryClientProvider>
   );
 }

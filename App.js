1ae425ceae5516e7061b452f05787359e12f9f64
James Almeida <james.almeida.ti@gmail.com>
2022-04-30 21:59:25 -0300

    Add online status management cfg

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+
+// UI
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
+
+// Navigation
 import { AppNavigator } from './src/Navigators/navigation.component';
 
+// Query
+import NetInfo from '@react-native-community/netinfo'
+import { onlineManager } from 'react-query'
 import { QueryClient, QueryClientProvider } from 'react-query'
+
+onlineManager.setEventListener(setOnline => {
+  return NetInfo.addEventListener(state => {
+    setOnline(state.isConnected)
+  })
+})
 
 export default function App() {
   const queryClient = new QueryClient()

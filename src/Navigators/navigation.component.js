0b7bf1bb9f8989703fae23ebadcda397c4d27fc0
Lucas Vieira <lucvieira.ti@gmail.com>
2022-05-21 21:59:41 -0300

    Create login page
    - Add navigation
    - Create component
    - Add inputs and submit button

--- src/Navigators/navigation.component.js
+++ src/Navigators/navigation.component.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { HomeScreen } from '../Components/home.component';
+import { LoginScreen } from '../Components/Login/login.component';
 import { RegisterPartyerScreen } from '../Components/RegisterPartyer/register-partyer.component';
 
 const { Navigator, Screen } = createStackNavigator();
 
-const HomeNavigator = () => (
+const LoginNavigator = () => (
   <Navigator screenOptions={{headerShown: false}}>
-    <Screen name='Home' component={HomeScreen}/>
+    <Screen name='Login' component={LoginScreen}/>
     <Screen name='RegisterPartyer' component={RegisterPartyerScreen} />
   </Navigator>
 );
 
 export const AppNavigator = () => (
   <NavigationContainer>
-    <HomeNavigator/>
+    <LoginNavigator/>
   </NavigationContainer>
 );

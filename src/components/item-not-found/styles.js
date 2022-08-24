e3000acd1f0a4a640904c10c79a0e2ba269bffeb
James Almeida <james.almeida.ti@gmail.com>
2022-08-24 16:51:53 -0300

    Fix authentication page
    - Add android structure files

--- src/components/item-not-found/styles.js
+++ src/components/item-not-found/styles.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native'
 
-export default styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   centerText: { textAlign: 'center' },
   container: {
     justifyContent: 'center',
